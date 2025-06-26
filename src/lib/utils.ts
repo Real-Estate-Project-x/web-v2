0e2d06743f1c28eef0421e6fdfc7a8635029e864
Abel Ani <abelanico6@gmail.com>
2025-06-26 15:51:15 +0100

    Update

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,17 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import axios from "axios";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+/**
+ * Get IP address of current_user on site
+ * @returns user ip_address
+ */
+export const getUserIp = async () => {
+  const url = "https://api.ipify.org/?format=json";
+  const userIPAddress = await axios<any, { ip: string }>(url);
+  return userIPAddress.ip;
+};
