beb42abac108b2a7b919691b216590d70ae405e3
Abel Ani <abelanico6@gmail.com>
2026-01-27 20:13:44 +0100

    Added deletion feature to fav-property

--- src/components/pages/User-dashboard/saved-properties.tsx
+++ src/components/pages/User-dashboard/saved-properties.tsx
@@ -1,5 +1,5 @@
-import { Button } from "@/components/ui/button";
 import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
 import { axiosInstance } from "@/lib/axios-interceptor";
 import {
   Card,
@@ -10,6 +10,7 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import Link from "next/link";
+import { toast } from "sonner";
 
 export const DashboardFavorites = () => {
   const [loading, setLoading] = useState(false);
@@ -17,25 +18,44 @@ export const DashboardFavorites = () => {
   const [favProperties, setFavProperties] = useState([]);
 
   useEffect(() => {
-    const fetchSavedProperties = async (pageNumber = 1, pageSize = 10) => {
-      try {
-        setLoading(true);
-        const url = `favourite-property?pageNumber=${pageNumber}&pageSize=${pageSize}`;
-        const result = await axiosInstance.get(url);
-        if (result?.data?.success) {
-          const { data, paginationControl } = result.data;
-          setFavProperties(data);
-          setTotalFavs(paginationControl.totalCount);
-        }
-      } catch (ex) {
-        console.error(ex);
-      } finally {
-        setLoading(false);
-      }
-    };
-
     fetchSavedProperties();
   }, []);
+
+  const fetchSavedProperties = async (pageNumber = 1, pageSize = 10) => {
+    try {
+      setLoading(true);
+      const url = `favourite-property?pageNumber=${pageNumber}&pageSize=${pageSize}`;
+      const result = await axiosInstance.get(url);
+      if (result?.data?.success) {
+        console.log({ result, rest: result.data.data });
+        const { data, paginationControl } = result.data;
+        setFavProperties(data);
+        setTotalFavs(paginationControl.totalCount);
+      }
+    } catch (ex) {
+      console.error(ex);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const removeFavProperty = async (propertyId: string) => {
+    const url = "/favourite-property";
+
+    try {
+      const response = await axiosInstance.delete(url, {
+        data: { propertyIds: [propertyId] },
+      });
+      if (response.data.success) {
+        toast.success(response.data.message);
+        fetchSavedProperties();
+      }
+      toast.info("Property removed from favorites");
+    } catch (error) {
+      console.log(error);
+      toast.error(`${error}`);
+    }
+  };
 
   return (
     <div>
@@ -46,10 +66,10 @@ export const DashboardFavorites = () => {
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {favProperties.map(({ property }: any) => (
-          <Card key={property} className="overflow-hidden p-0">
+          <Card key={property.id} className="overflow-hidden p-0">
             <div className="h-60 overflow-hidden bg-gray-200">
               <img
-                src={property.propertyImages[0].url}
+                src={property.propertyImages[0]?.image.url}
                 alt=""
                 className="h-full w-full object-cover"
               />
@@ -78,45 +98,11 @@ export const DashboardFavorites = () => {
               >
                 View Details
               </Link>
-              <Button onClick={() => {}} variant="ghost" size="sm">
-                Remove
-              </Button>
-            </CardFooter>
-          </Card>
-        ))}
-
-        {[0, 1].map(({ property }: any) => (
-          <Card key={property} className="overflow-hidden p-0">
-            <div className="h-60 overflow-hidden bg-gray-200">
-              <img
-                src="https://images.pexels.com/photos/14352080/pexels-photo-14352080.jpeg"
-                alt=""
-                className="h-full w-full object-cover"
-              />
-            </div>
-            <CardHeader className="p-4">
-              <CardTitle className="text-lg">3 Bedroom flat</CardTitle>
-              <CardDescription>₦500,000</CardDescription>
-            </CardHeader>
-            <CardContent className="p-4 pt-0">
-              <p className="text-sm">
-                For sale • 3 bedrooms • 2 bathrooms • 2 cars • 2 garages
-              </p>
-            </CardContent>
-
-            <CardFooter className="p-4 border-t flex justify-between">
-              <Link
-                style={{
-                  border: "1px solid #ccc",
-                  borderRadius: "10px",
-                  padding: "7px 10px",
-                }}
-                href={`properties/view?id=${property?.slug}`}
-                target="_blank"
+              <Button
+                onClick={() => removeFavProperty(property.id)}
+                variant="ghost"
+                size="sm"
               >
-                View Details
-              </Link>
-              <Button variant="ghost" size="sm">
                 Remove
               </Button>
             </CardFooter>
