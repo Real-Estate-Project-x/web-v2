28dbff450c94e6b66c8f6d56f16fcace447fa4d9
Abel Ani <abelanico6@gmail.com>
2026-02-24 14:54:28 +0100

    Update

--- utils/interfaces.ts
+++ utils/interfaces.ts
@@ -412,7 +412,7 @@ export interface SearchPropertyInterfaceType {
       isRegistered: boolean;
       inPersonViewingFee: number | null;
     };
-    // photoUrls: string[];
+    propertyImages: [];
     postedByUser: {
       id: string;
       status: boolean;
