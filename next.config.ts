f68f0fdafad9c658ebdfe150a4fa83165872fa4f
Ruth181 <ruthani124@gmail.com>
2026-03-30 15:05:21 +0100

    added pwa function

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,17 @@
+import withPWAInit  from "@ducanh2912/next-pwa";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "export",
-  /* config options here */
+const withPwa = withPWAInit({
+  dest : "public",
+  cacheOnFrontEndNav : true,
+  aggressiveFrontEndNavCaching : true,
+  reloadOnOnline : true,
+  disable : process.env.NODE_ENV === "development",
+});
 
-  images: {
+export default withPwa({
+  output : "export",
+  images : {
     unoptimized: true,
     remotePatterns: [
       {
@@ -39,6 +46,46 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-};
+});
+// const nextConfig: NextConfig = {
+//   output: "export",
+//   /* config options here */
 
-export default nextConfig;
+//   images: {
+//     unoptimized: true,
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "images.unsplash.com",
+//         port: "",
+//         pathname: "/**",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "randomuser.me",
+//         port: "",
+//         pathname: "/**",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "via.placeholder.com",
+//         port: "",
+//         pathname: "/**",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "cdn.pixabay.com",
+//         port: "",
+//         pathname: "/**",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "images.pexels.com",
+//         port: "",
+//         pathname: "/**",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
